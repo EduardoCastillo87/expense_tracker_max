2ce1d25050f3a3b40f718e46e9f1f55ce7766ac1
Eduardo Castillo <eduardo.infoline@gmail.com>
2025-05-24 08:35:39 -0500

    Add NewExpense widget and integrate it into the Expenses overlay for adding expenses

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -1,3 +1,4 @@
+import 'package:expense_tracker_max/widgets/new_expense.dart';
 import 'package:flutter/material.dart';
 import 'package:expense_tracker_max/models/expense.dart';
 import 'package:expense_tracker_max/widgets/expenses_list/expenses_list.dart';
@@ -28,10 +29,7 @@ class _ExpensesState extends State<Expenses> {
   ];
 
   void _openAddExpenseOverlay() {
-    showModalBottomSheet(
-      context: context,
-      builder: (ctx) => Text('Modal botton sheet'),
-    );
+    showModalBottomSheet(context: context, builder: (ctx) => NewExpense());
   }
 
   @override
