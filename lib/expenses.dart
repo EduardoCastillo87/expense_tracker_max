e70e7ba19d68796a1cc7dd8710805ae7fc70eb39
Eduardo Castillo <eduardo.infoline@gmail.com>
2025-05-23 20:28:09 -0500

    Add initial registered expenses to Expenses widget

--- lib/expenses.dart
+++ lib/expenses.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:expense_tracker_max/models/expense.dart';
 
 class Expenses extends StatefulWidget {
   const Expenses({super.key});
@@ -10,6 +11,15 @@ class Expenses extends StatefulWidget {
 }
 
 class _ExpensesState extends State<Expenses> {
+  final List<Expense> _registeredExpenses = [
+    Expense(
+      title: 'flutter course',
+      amount: 19.99,
+      date: DateTime.now(),
+      category: Category.work,
+    ),
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
