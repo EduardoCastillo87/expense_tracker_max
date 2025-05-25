bf9ae4144c3e1c0b0c396111c3aad0e2e8d25e1f
Eduardo Castillo <eduardo.infoline@gmail.com>
2025-05-25 11:11:29 -0500

    Implement custom color scheme in main app and enhance expense management with undo functionality in Expenses widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:expense_tracker_max/widgets/expenses.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 150, 80, 230),
+);
+
 void main() {
   runApp(
-    MaterialApp(theme: ThemeData(useMaterial3: true), home: const Expenses()),
+    MaterialApp(
+      theme: ThemeData().copyWith(colorScheme: kColorScheme),
+      home: const Expenses(),
+    ),
   );
 }
